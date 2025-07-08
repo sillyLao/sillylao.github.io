73b33b95ad8c1eeeb2b06f24cf21e03df4ec2f2a
sillyLao <Laonyyyx@gmail.com>
2025-07-08 05:32:42 +0200

    + peg solitaire + test ajuster le viewport

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-var listGames = ["k&pf", "GOL Web"]
-var randomGame = listGames[Math.floor(Math.random() * listGames.length)]
-document.getElementById("game").src = "https://sillylao.github.io/webgames/" + randomGame + "/index.html"
+var listGames = ["k&pf", "GOL Web", "PEG Solitaire Web"]
+var listWidth = ["1067", "800", "800"]
+var listHeight = ["606", "800", "800"]
+var randomInt = Math.floor(Math.random() * listGames.length)
+var Game = listGames[randomInt]
+document.getElementById("game").src = "https://sillylao.github.io/webgames/" + Game + "/index.html"
+var Width = listWidth[randomInt]
+document.getElementById("game").width = Width
+var Height = listHeight[randomInt]
+document.getElementById("game").height = Height
 
 
