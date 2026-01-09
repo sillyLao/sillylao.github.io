7c53b9547273c2634cddb0df9542e04db24d2a52
sillyLao <Laonyyyx@gmail.com>
2026-01-09 23:22:02 +0100

    kicya

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-var listGames = ["k&pf", "GOL Web", "PEG Solitaire Web"]
-var listWidth = ["1067", "800", "800"]
-var listHeight = ["606", "800", "800"]
+var listGames = ["k&pf", "GOL Web", "PEG Solitaire Web", "Miniling Jumper Web"]
+var listWidth = ["1067", "800", "30vw", "450"]
+var listHeight = ["606", "800", "30vh", "800"]
 var randomInt = Math.floor(Math.random() * listGames.length)
 var Game = listGames[randomInt]
 document.getElementById("game").src = "https://sillylao.github.io/webgames/" + Game + "/index.html"
